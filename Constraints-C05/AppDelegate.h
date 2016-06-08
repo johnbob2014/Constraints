43376e8d16f9d4141988d1f575d3261ff297c8b2
johnbob2014 <johnbob2014@icloud.com>
2016-06-08 11:09:01 +0800

    Initial Commit

---
+++ Constraints-C05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Constraints-C05
+//
+//  Created by BobZhang on 16/6/8.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Constraints-C05/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Constraints-C05
+//
+//  Created by BobZhang on 16/6/8.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
